84844fba092ea0d4327f889e3c2179c6570700d5
Brandon Hansen <bh@jesusculture.com>
2014-07-07 15:14:47 -0700

    Adding config files

--- lib/ooyala.rb
+++ lib/ooyala.rb
@@ -4,4 +4,21 @@ require 'ooyala/live_stream.rb'
 
 module Ooyala
 
+  class Config
+
+    @@file = File.join(File.dirname(File.expand_path(File.dirname(__FILE__))), 'keys.yml')
+
+    def self.fetch(name)
+      @config ||= YAML.load_file(@@file)
+      @config[name] if @config
+    end
+
+    def self.set(name, val)
+      data = YAML.load_file(@@file) || Hash.new
+      data[name] = val
+      File.open(@@file, "w") { |f| YAML.dump(data, f) }
+    end
+
+  end
+
 end
